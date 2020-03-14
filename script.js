30b7d474edc5f74efb31eaeb1a6f3eaf642ee2f6
Jeremy N <no-reply>
2020-03-13 17:16:38 -0700

    got the setter and getter to work now need to make the past/future work

--- script.js
+++ script.js
@@ -3,28 +3,46 @@ $(document).ready(function () {
     var blocks = document.querySelectorAll(".row");
     var timeBlock = document.querySelectorAll(".timeblock");
     var submit = document.querySelectorAll(".saveBtn");
-  
-    var details = localStorage.getItem("details");
+    var textBox = document.querySelectorAll("description");
+    textBox.textContent = localStorage.getItem("data-time");
+    var hours = ["9am", "10am", "11am"];
+    // current time function
     var currentDay = moment().format("MMM-DD-YY");
     $("#currentDay").text(currentDay);
 
-    // $(inputBox).on("click", function () {
-    //     $(this).replaceWith("<input id=\"input\">");
-    // });
 
 
+    // calling function to retrieve data
+
+
+    //  code block to store data
     $(submit).on("click", function () {
-        var time =$(this).attr("data-time")
+        var time = $(this).attr("data-time")
         var inputBox = $("#" + time);
         var details = inputBox.val()
-      
+
         localStorage.setItem(time, details)
+
+        //  attempt to retrieve stored data
+
+
     })
 
+    function latestInfo() {
+        for (i = 0; i < hours.length; i++) {
+            console.log(localStorage.getItem(hours[i]))
+            $("#" + hours[i]).text(localStorage.getItem(hours[i]));
+        }
+        //    textBox.textContent = 
+        //    console.log(localStorage.getItem(details));
+
+    }
+
+    latestInfo();
     // function timeCheck (){
 
 
-    // if( timeBlock.text < moment()){
+    // if( timeBlock.innertext < moment()){
     //      blocks.addClass("past");
     // };
     // else if (timeBlock.text > moment()){
@@ -36,5 +54,14 @@ $(document).ready(function () {
 
     // };
 
-//     console.log(timeCheck);
+
+
+
+    //  different attempts to get stored data
+
+    // $("9am").val(localStorage.getItem("9"));
+
+    // $("textBox").val(localStorage.getItem("9"));
+    // $("data-time").val(localStorage.getItem(latestInfo));
+    // $("input10").val(localStorage.getItem("10"));
 });
